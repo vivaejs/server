4e8ff3992bef8745645d005fffbcf369ed39efc9
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-05 22:19:45 -0700

    Simplify types

--- lib/plugins/index.d.ts
+++ lib/plugins/index.d.ts
@@ -1,23 +1,10 @@
-import type { VivaeServer, Middleware, Method } from "../index";
+import type { Middleware, Method } from "../index";
 
 export interface Plugin {
-  plugin: true;
-  init(server: VivaeServer): Route;
-}
-
-export interface Route {
   path?: string;
   method?: Method;
   middleware: Middleware;
 }
-
-type PluginFunction<Args extends any[]> = (
-  ...args: Args
-) => (server: VivaeServer) => Route;
-
-export function createPlugin<Args extends any[]>(
-  fn: PluginFunction<Args>,
-): (...args: Args) => Plugin;
 
 export function serve(options?: {
   directory?: string;
